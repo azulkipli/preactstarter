a86b546fabba704f702e86768cd7e3f426eaf930
azulkipli <azulkipli@gmail.com>
2018-03-27 13:56:13 +0700

    include spectrecss

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-// import "blaze/scss/dist/blaze.min.css"; // all blaze css
-import "blaze/scss/dist/generics.global.min.css";
-import "blaze/scss/dist/blaze.colors.min.css";
-import "blaze/scss/dist/objects.grid.min.css";
-import "blaze/scss/dist/objects.grid.responsive.min.css";
-import "blaze/scss/dist/components.typography.min.css";
-import "blaze/scss/dist/components.buttons.min.css";
-import "blaze/scss/dist/components.overlays.min.css";
-import "blaze/scss/dist/objects.drawers.min.css";
-import "blaze/scss/dist/utilities.alignment.min.css";
-import "blaze/scss/dist/utilities.boxing.min.css";
-import "blaze/scss/dist/utilities.sizes.min.css";
-import "blaze/scss/dist/components.cards.min.css";
-import "blaze/scss/dist/components.headings.min.css";
-import "blaze/scss/dist/components.avatars.min.css";
-import "blaze/scss/dist/objects.modals.min.css";
+import "spectre.css/dist/spectre.min.css";
+import "spectre.css/dist/spectre-exp.min.css";
 
-import "./style";
+// import "./style";
 
 import App from "./components/app";
 
